a1ae2dba798dec2b5c11cecd12e254908f33eb8f
Olivier Boeren <olivierboeren@live.nl>
2025-02-09 15:33:40 +0100

    add: download script

--- download.lua
+++ download.lua
@@ -1,10 +1,11 @@
 local shell = require('shell')
 
+local repo = 'https://raw.githubusercontent.com/KerelOlivier/cropbreeding/master/'
 local files={
 	'config.lua',
 	'autoStat.lua'
 }
 
 for i=1, #files do
-    shell.execute(string.format('wget -f %s%s/%s', repo, branch, scripts[i]))
+    shell.execute(string.format('wget -f %s/%s', repo, files[i]))
 end
