8ebb47e55ab7c523063fb4d2e637445a4f3f98f6
Olivier Boeren <olivierboeren@live.nl>
2025-02-09 15:44:04 +0100

    add: loop for autostat

--- autoStat.lua
+++ autoStat.lua
@@ -1,5 +1,16 @@
+local robot = require('robot')
+local config = require('config')
+
+local function patrol()
+	print("patrolling")
+	for i=1, config.workingFarmArea do
+		print("moving", i)
+	end
+end
+
 local function main()
-	print("...")
+	print("Starting auto stat")
+	patrol()
 end
 
 main()
--- config.lua
+++ config.lua
@@ -1,4 +1,12 @@
 local config = {
 	storageFarmSize = 9,
 	workingFarmSize = 6,
+
+	storageFarmArea = 0,
+	workingFarmArea = 0,
 }
+
+config.storageFarmArea = config.storageFarmSize^2
+config.workingFarmArea = config.workingFarmSize^2
+
+return config
