de4ad2c7f3f2fe350d37dd12d7aba2cf8af4c27d
Olivier Boeren <olivierboeren@live.nl>
2025-02-09 16:09:38 +0100

    fix: looping behaviour

--- autoStat.lua
+++ autoStat.lua
@@ -6,7 +6,12 @@ local function patrol()
 	local farmSpot = 1
 	while farmSpot < config.workingFarmArea do
 		while not robot.forward() do end
-		if farmSpot%config.workingFarmSize == 0 then
+		if farmSpot%(config.workingFarmArea*2) == (config.workingFarmSize*2)-1 then
+			print("turn right")
+			robot.turnRight()
+			robot.foward()
+			robot.turnRight()
+		elseif farmSpot%config.workingFarmSize == config.workingFarmSize-1 then
 			print("turn")
 			robot.turnLeft()
 			robot.forward()
